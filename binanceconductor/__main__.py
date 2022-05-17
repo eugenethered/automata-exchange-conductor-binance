ad71d082f06f9f0ad3f312e806404b759bc9c6e4
Eugene the Red <me@eugenethered.com>
2022-05-17 21:59:25 +0100

    polishing main

--- binanceconductor/__main__.py
+++ binanceconductor/__main__.py
@@ -6,7 +6,8 @@ from core.arguments.command_line_arguments import url_option_arg_parser
 
 from binanceconductor.BinanceExchangeConductor import BinanceExchangeConductor
 
-if __name__ == '__main__':
+
+def main():
     command_line_arg_parser = url_option_arg_parser()
     args = command_line_arg_parser.parse_args()
 
@@ -20,3 +21,7 @@ if __name__ == '__main__':
 
     conductor = BinanceExchangeConductor(args.url, args.options)
     conductor.receive_data()
+
+
+if __name__ == '__main__':
+    main()
